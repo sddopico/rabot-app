7a2e9eb7c19d44a6852ee021b07799df5f04d324
Dominic Dopico <serafin.dopico@gmail.com>
2020-04-16 15:40:40 -0700

    generate SchedulerComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { SchedulerComponent } from './scheduler/scheduler.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SchedulerComponent
   ],
   imports: [
     BrowserModule,
