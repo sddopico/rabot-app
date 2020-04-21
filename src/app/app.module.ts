2a8bd475bf856754f9b353bf7ea60aa19b2db3de
Dominic Dopico <serafin.dopico@gmail.com>
2020-04-21 15:11:27 -0700

    handle login on auth service | register angular fire libraries in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { environment } from "../environments/environment";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 
 // App Modules
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { SchedulerComponent } from './scheduler/scheduler.component';
-import { EventComponent } from './event/event.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { auth } from 'firebase/app';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SharedModule } from "./shared/shared.module";
+import { SchedulerComponent } from "./scheduler/scheduler.component";
+import { EventComponent } from "./event/event.component";
+import { HomePageComponent } from "./home-page/home-page.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     SchedulerComponent,
     EventComponent,
-    HomePageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-    auth,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   exports: [AppComponent],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+
+declare var gapi: any;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  user$: Observable<firebase.User>;
+  calendarItems: any[];
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth) {
+    this.initClient();
+    this.user$ = afAuth.authState;
+  }
+
+  initClient() {
+    gapi.load('client', () => {
+      gapi.client.init({
+        apiKey: 'AIzaSyDcRMWorUdVFSbcyk2wSdfxFsP2CzxSHsA',
+        clientId:
+          '957272507951-kd7rmumu4q3bpdtbhhntej4vu868s6ta.apps.googleusercontent.com',
+        discoveryDocs: [
+          'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
+        ],
+        scope: 'https://www.googleapis.com/auth/calendar',
+      });
+
+      gapi.client.load('calendar', 'v3', () => console.log('loaded calendar'));
+    });
+  }
+
+  async login() {
+    const googleAuth = gapi.auth2.getAuthInstance();
+    const googleUser = await googleAuth.signIn();
+
+    const token = googleUser.getAuthResponse().id_token;
+
+    console.log(googleUser);
+
+    const credential = auth.GoogleAuthProvider.credential(token);
+
+    await this.afAuth.signInAndRetrieveDataWithCredential(credential);
+  }
+
+  logout() {
+    this.afAuth.signOut();
+  }
 }
