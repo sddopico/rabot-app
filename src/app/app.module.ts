9d7b01718eb157ab57858b1448336bc74c9f1951
Dominic Dopico <serafin.dopico@gmail.com>
2020-04-22 13:52:17 -0700

    Revert "handle login on auth service | register angular fire libraries in app.module"
    This reverts commit 2a8bd475bf856754f9b353bf7ea60aa19b2db3de.
    
    roll back auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,32 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { environment } from "../environments/environment";
-import { AngularFireAuthModule } from "@angular/fire/auth";
-import { AngularFireModule } from "@angular/fire";
-import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // App Modules
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { SharedModule } from "./shared/shared.module";
-import { SchedulerComponent } from "./scheduler/scheduler.component";
-import { EventComponent } from "./event/event.component";
-import { HomePageComponent } from "./home-page/home-page.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { SchedulerComponent } from './scheduler/scheduler.component';
+import { EventComponent } from './event/event.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { auth } from 'firebase/app';
 
 @NgModule({
   declarations: [
     AppComponent,
     SchedulerComponent,
     EventComponent,
-    HomePageComponent
+    HomePageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    AngularFireAuthModule
+    auth,
   ],
   exports: [AppComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
